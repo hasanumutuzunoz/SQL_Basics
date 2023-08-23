6784a31c9c2f6fa1df19c780c7235fdafc6d2c61
Hasan <hasanumutuzunoz@gmail.com>
2023-08-22 20:23:45 -0700

    Uploading the rest of the file

--- SQL_Server/SQLIntermediateTutorial.sql
+++ SQL_Server/SQLIntermediateTutorial.sql
@@ -1,6 +1,25 @@
 SELECT * FROM SQLTutorial.dbo.EmployeeDemographics;
 SELECT * FROM SQLTutorial.dbo.EmployeeSalary;
 
+Insert into EmployeeDemographics VALUES
+(1011, 'Ryan', 'Howard', 26, 'Male'),
+(NULL, 'Holly', 'Flax', NULL, NULL),
+(1013, 'Darryl', 'Philbin', NULL, 'Male')
+
+Create Table WareHouseEmployeeDemographics 
+(EmployeeID int, 
+FirstName varchar(50), 
+LastName varchar(50), 
+Age int, 
+Gender varchar(50)
+)
+
+Insert into WareHouseEmployeeDemographics VALUES
+(1013, 'Darryl', 'Philbin', NULL, 'Male'),
+(1050, 'Roy', 'Anderson', 31, 'Male'),
+(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+(1052, 'Val', 'Johnson', 31, 'Female')
+
 -- JOINS
 SELECT * FROM SQLTutorial.dbo.EmployeeDemographics
 JOIN SQLTutorial.dbo.EmployeeSalary
@@ -23,5 +42,126 @@ INNER JOIN SQLTutorial.dbo.EmployeeSalary
 ON SQLTutorial.dbo.EmployeeDemographics.EmployeeID = SQLTutorial.dbo.EmployeeSalary.EmployeeID;
 
 
+-- UNIONS
+SELECT * FROM SQLTutorial.dbo.EmployeeDemographics
+FULL OUTER JOIN SQLTutorial.dbo.WareHouseEmployeeDemographics
+ON EmployeeDemographics.EmployeeID = WareHouseEmployeeDemographics.EmployeeID;
+
+
+SELECT * FROM SQLTutorial.dbo.EmployeeDemographics
+UNION ALL		 													-- UNION remover all duplicates,   -- UNION ALL includes duplicates
+SELECT * FROM SQLTutorial.dbo.WareHouseEmployeeDemographics
+ORDER BY EmployeeID
+
+SELECT EmployeeID, FirstName, Age FROM SQLTutorial.dbo.EmployeeDemographics
+UNION	 											
+SELECT EmployeeID, JobTitle, Salary FROM SQLTutorial.dbo.EmployeeSalary
+ORDER BY EmployeeID
+
+
+-- CASE STATEMENT
+SELECT FirstName, LastName, Age, 
+CASE 
+	WHEN Age > 30 THEN 'Old'
+	WHEN Age BETWEEN 27 AND 30 THEN 'Young'
+	WHEN Age = 38 THEN 'Stanley'   -- only the first code runs so this won't work unless it is on top
+	ELSE 'Baby'
+END
+FROM SQLTutorial.dbo.EmployeeDemographics
+WHERE Age IS NOT NULL
+ORDER BY Age	
+
+
+SELECT FirstName, LastName, JobTitle, Salary,
+CASE
+	WHEN JobTitle = 'Salesman' THEN Salary + (Salary * .10)
+	WHEN JobTitle = 'Accountant' THEN Salary + (Salary * .05)
+	WHEN JobTitle = 'HR' THEN Salary + (Salary * .0001)
+	ELSE Salary + (Salary * .03)
+END AS SalaryAfterRaise
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID;
+
+
+-- HAVING CLAUSE
+SELECT JobTitle, COUNT (JobTitle)
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+HAVING COUNT(JobTitle) > 1
+
+
+SELECT JobTitle, AVG(Salary)
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+	ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+HAVING AVG(Salary) > 45000
+ORDER BY AVG(Salary)
+
+
+-- UPDATING / DELETING DATA
+SELECT * FROM SQLTutorial.dbo.EmployeeDemographics
+
+UPDATE SQLTutorial.dbo.EmployeeDemographics
+SET EmployeeID = 1012
+WHERE FirstName = 'Holly' AND LastName = 'Flax'
+
+UPDATE SQLTutorial.dbo.EmployeeDemographics
+SET Age = 31, Gender = 'Female'
+WHERE FirstName = 'Holly' AND LastName = 'Flax'
+
+DELETE FROM SQLTutorial.dbo.EmployeeDemographics
+WHERE EmployeeID = 1005
+
+SELECT *    -- It is better to check first which row are we deleting because we cannot get back what is deleted
+FROM SQLTutorial.dbo.EmployeeDemographics
+WHERE EmployeeID = 1004
+
+
+-- Aliasing
+SELECT FirstName AS Fname
+FROM SQLTutorial.dbo.EmployeeDemographics
+
+SELECT FirstName Fname
+FROM SQLTutorial.dbo.EmployeeDemographics
+
+SELECT FirstName + ' ' + LastName AS FullName
+FROM SQLTutorial.dbo.EmployeeDemographics
+
+SELECT AVG(Age) AS AvgAge
+FROM SQLTutorial.dbo.EmployeeDemographics
+
+SELECT Demo.EmployeeID, Sal.Salary
+FROM SQLTutorial.dbo.EmployeeDemographics AS Demo    -- Aliasing table name is useful to not write the full path name
+JOIN SQLTutorial.dbo.EmployeeSalary AS Sal			-- it has to be related with the table name instead of a b c etc. otherwise it will be confusing when working with multible joins
+	ON Demo.EmployeeID = Sal.EmployeeID
+
+
+-- PARTITION BY
+SELECT * FROM SQLTutorial..EmployeeDemographics
+SELECT * FROM SQLTutorial..EmployeeSalary
+
+SELECT FirstName, LastName, Gender, Salary, 
+COUNT(Gender) OVER (PARTITION BY Gender) AS TotalGender   -- PARTITION BY takes gender count into one line, you can compare it with GROUP BY below
+FROM SQLTutorial..EmployeeDemographics dem				  -- so if I want to add how many total males and females to each column in this example, I need to use PARTITION BY
+JOIN SQLTutorial..EmployeeSalary sal					  -- because GROUP BY will not return for all columns
+	ON dem.EmployeeID = sal.EmployeeID
+
+
+SELECT FirstName, LastName, Gender, Salary, COUNT(Gender)
+FROM SQLTutorial..EmployeeDemographics dem
+JOIN SQLTutorial..EmployeeSalary sal
+	ON dem.EmployeeID = sal.EmployeeID
+GROUP BY FirstName, LastName, Gender, Salary
+
+
+SELECT Gender, COUNT(Gender)
+FROM SQLTutorial..EmployeeDemographics dem
+JOIN SQLTutorial..EmployeeSalary sal
+	ON dem.EmployeeID = sal.EmployeeID
+GROUP BY Gender
 
 
